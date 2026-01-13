824b46d71349b82786c088873c805b48e037fa9d
Gali1917 <carlgale93@hotmail.com>
2026-01-13 15:00:26 -0500

    Add/Gali1917/Portada, mas productos

--- src/components/Ad.jsx
+++ src/components/Ad.jsx
@@ -1,18 +1,17 @@
 import React from "react";
+import productData from "../data/products.json";
+
 import "../styles/ad.css";
 
 const Ad = () => {
   return (
     <div className="home-page-ad-container">
-      <img
-        src="https://res.cloudinary.com/dd8a6qc45/image/upload/v1767057294/EcoMovilidad/products/HALC%C3%93N_EB-4_vw2ut8.jpg"
-        alt="Foto portada"
-      />
+      <img src={productData[0].imagenes[0]} alt={productData[0].nombre} />
       <div className="home-page-ad-text-container">
-        <h2>HALCÓN EB-4</h2>
+        <h2>{productData[0].nombre}</h2>
         <p>
-          Eléctrica <br />4 Asientos <br />
-          Autonomía 120 km
+          {productData[0].descripcion_corta} <br />
+          Autonomia de {productData[0].especificaciones.autonomia} <br />
         </p>
         <button className="button-home-page-ad-container">Comprar</button>
       </div>
